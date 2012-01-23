758a839752ac3ac647091790c5ed9547eaf8095e
Edgars Jekabsons <edgars.jekabsons@tieto.com>
2012-01-23 14:50:11 +0200

    Added SQL logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Diary::Application.configure do
+  config.logger = Logger.new(STDOUT)
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
