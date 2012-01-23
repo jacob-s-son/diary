18f367e38255ad3dfe9a54c86e7c241ada2740d9
Edgars Jekabsons <edgars.jekabsons@tieto.com>
2012-01-23 12:03:05 +0200

    Fix for Postgre Time format

--- app/models/note.rb
+++ app/models/note.rb
@@ -10,6 +10,6 @@ class Note < ActiveRecord::Base
   attr_accessible :title, :description, :planned_time
   
   scope :user_notes_for_two_days, lambda {|user, day|
-    where ["user_id = ? AND planned_time >= ? AND planned_time < ?", user, day, day+2 ]
+    where ["user_id = ? AND planned_time >= ? AND planned_time < ?", user, day.to_time, (day+2).to_time ]
   }
 end
